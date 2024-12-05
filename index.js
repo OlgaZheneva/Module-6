6f6db5628e628582559767d2779192efa54f28f5
OlgaZheneva <olgazhenevasokol@gmail.com>
2024-12-05 18:36:55 +0300

    создан файл style.css

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const str = 'Olga';
+const str = 'Эльбрус';
+let newStr = '';
+
+// Цикл для реверса строки
+for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i]; // добавляем символ в новый строку
+}
+
+// Вывод результирующей реверсированной строки
+console.log('reversed:', newStr);
+
+function reverse(str) {
+    let newStr = '';
+    
+    // Цикл для реверса строки
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i]; // добавляем каждый символ в новую строку
+    }
+    
+    return newStr; // возвращаем перевернутую строку
+}
+
+// Пример использования
+const originalString = 'Эльбрус';
+const reversedString = reverse(originalString);
+console.log('Перевернутая строка:', reversedString); // Перевернутая строка: сурблЭ 
